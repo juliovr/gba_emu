5f64a5a311edb4a1c3fa57b4f3722fe1a8280f7c
Julio Vidaurre <julio.vidaurre@gmail.com>
2025-01-16 14:59:59 -0300

    Fix windows build by adding no warnings on CRT.

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 @echo off
 
 set common_compiler_flags=-Od -fp:fast -nologo -MT -Oi -Gm- -GR- -EHa- -W4 -WX -wd4201 -wd4100 -wd4189 -wd4505 -wd4456 -wd4459 -wd4311 -wd4312 -wd4302 -wd4706 -wd4127 -FC -Z7
-set common_compiler_flags=-D_DEBUG %common_compiler_flags%
+set common_compiler_flags=-D_DEBUG -D_CRT_SECURE_NO_WARNINGS %common_compiler_flags%
 
 IF NOT EXIST bin mkdir bin
 pushd bin
